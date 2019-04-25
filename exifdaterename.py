864cd6e3ab3a2f0c35d458b1564453a3cdadb55c
Dale Kim <dtkim1@gmail.com>
2019-04-24 21:58:52 -0700

    Fixing a bug where new file could be written to wrong location.
    If the file paths given to the script are leaf file names, then the
    script would incorrectly build the new file path and put the new file at
    the root of the drive rather than in the same directory as the original
    file.
    
    This occurred because the original file path was parsed to extract the
    directory portion and the leaf portion then use the directory as the
    base for the renamed file.  However, if you supply only the leaf to
    begin with, directory is an empty string so you end up with "/leaf.ext".

--- exifdaterename.py
+++ exifdaterename.py
@@ -3,6 +3,12 @@ import sys
 import os
 
 for file_path in sys.argv[1:]:
+    # The absolute path should be retrieved to ensure that we
+    # stick the renamed file in the same directory as the original
+    # image.  If file_path is something like "DSC_001.NEF" then
+    # dir is the empty string and new_file_path below is "/YYYY-MM-DD...NEF"
+    # and the new file will just be at the root of the current drive.
+    file_path = os.path.abspath(file_path)
     dir, filename = os.path.split(file_path)
     basename, extension = os.path.splitext(filename)
     f = open(file_path, 'rb')
